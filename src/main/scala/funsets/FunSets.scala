72b4c9d51de050320b370be66faa36c34c947c53
Igor Glizer <igorg@wix.com>
2014-07-28 14:06:38 +0300

    map and exists refactured

--- src/main/scala/funsets/FunSets.scala
+++ src/main/scala/funsets/FunSets.scala
@@ -66,12 +66,12 @@ object FunSets {
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, x => !p(x) && true)
+  def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, x => !p(x))
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: Set, f: Int => Int): Set = x => (-bound to bound).filter(contains(s, _)).map(f).contains(x)
+  def map(s: Set, f: Int => Int): Set = x => exists(s, y => y == f(x))
 
   /**
    * Displays the contents of a set
